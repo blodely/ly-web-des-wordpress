4301096e29cb07da5ba49d88c5ccc1b49698b057
Luo Yu (Server) <indie.luo@gmail.com>
2017-09-08 13:17:45 +0800

    [Modify] cnzz id changed

--- fall-by-mr-luo/footer.php
+++ fall-by-mr-luo/footer.php
@@ -16,7 +16,7 @@
 <div class="site-info">
 Copyright &copy; Luo Yu, Powered by&nbsp;<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//blog.luoyu.space/design/theme-fall" title="Theme Fall">Theme Fall by 骆昱(Luo Yu)</a>, <?php echo date('Y'); ?>.
 </div><!-- .site-info -->
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1261172731'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1261172731%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>		
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1263041771'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1263041771%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
 
 </footer><!-- #colophon -->
 </div><!-- #page -->
